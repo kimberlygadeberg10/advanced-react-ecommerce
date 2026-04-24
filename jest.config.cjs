1d36c7d9a8f5acb1de7c0b3fe36339beb68f93e7
Kimberly Gadeberg <kimberlygadeberg10@gmail.com>
2026-04-24 12:46:29 -0500

    chore: set up jest and react testing library

---
+++ jest.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\\.(svg|png|jpg|jpeg|gif|webp)$": "<rootDir>/src/test/fileMock.ts",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.app.json",
+      },
+    ],
+  },
+};
